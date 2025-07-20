5db176c79679ee8394808ad0283f2d81be97d62d
krsticzoran <zorankrstic81@gmail.com>
2025-07-20 22:12:11 +0200

    style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+
 import { Geist, Geist_Mono, Jost, EB_Garamond } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/layout/header";
